0e6180f2d1294f451f8f210a7022f54274164314
yyjjvv <jiwonyoo66@gmail.com>
2024-01-06 01:17:01 +0900

    submit qrcode function

--- pages/qrcodes/new.js
+++ pages/qrcodes/new.js
@@ -1,17 +1,24 @@
-import Head from 'next/head';
-import QRCodeForm from '@/components/QRCodeForm';
-import styles from '@/styles/QRCodeCreatePage.module.css';
+import Head from "next/head";
+import QRCodeForm from "@/components/QRCodeForm";
+import styles from "@/styles/QRCodeCreatePage.module.css";
+import axios from "@/lib/axios";
+import { useRouter } from "next/router";
 
 export default function QRCodeCreatePage() {
-  return (
-    <>
-      <Head>
-        <title>새 QRCode 추가 - Shortit</title>
-      </Head>
-      <div className={styles.page}>
-        <h1 className={styles.title}>새 QRCode 추가</h1>
-        <QRCodeForm />
-      </div>
-    </>
-  );
+    const router = useRouter();
+    const handleSubmit = async (values) => {
+        await axios.post("/qrcodes", values);
+        router.push("/qrcodes");
+    };
+    return (
+        <>
+            <Head>
+                <title>새 QRCode 추가 - Shortit</title>
+            </Head>
+            <div className={styles.page}>
+                <h1 className={styles.title}>새 QRCode 추가</h1>
+                <QRCodeForm onSubmit={handleSubmit} />
+            </div>
+        </>
+    );
 }
